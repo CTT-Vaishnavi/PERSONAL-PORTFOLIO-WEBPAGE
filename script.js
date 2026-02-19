05e52927ea41be5be9c8ce2a65656e159025e0bf
vaishnavi <shindevai2004@gmail.com>
2026-02-19 21:53:06 +0530

    save changes

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("sub").addEventListener("click", function(event) {
+                        event.preventDefault();
+                        alert("Thank you for subscribing to the newsletter!");
+                    });

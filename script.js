2f2990813dbeeea435ec83df3308cbee02ebfcc4
vaishnavi <shindevai2004@gmail.com>
2026-02-20 20:28:25 +0530

    footer section

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.getElementById("sub").addEventListener("click", function(event) {
                         event.preventDefault();
                         alert("Thank you for subscribing to the newsletter!");
-                    });
+});
+                    
+document.getElementById("")

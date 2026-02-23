d7d7b61c4c0ede1104ac9b9d9b6bbc265b9f0fba
vaishnavi <shindevai2004@gmail.com>
2026-02-23 21:32:07 +0530

    save code

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ document.getElementById("sub").addEventListener("click", function(event) {
 });
                     
 document.getElementById("")
+
+
+    const menuToggle = document.getElementById("menu-toggle");
+    const navLinks = document.getElementById("nav-links");
+
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
